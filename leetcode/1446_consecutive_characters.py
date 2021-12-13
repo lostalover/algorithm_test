23f1e68e3ac849bed1879e3a881a17c5a033b7a0
lostalover <hdzzang93@gmail.com>
2021-12-13 21:44:04 +0900

    leetcode 1446

---
+++ leetcode/1446_consecutive_characters.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_count = 0
+        count = 1
+        if len(s) >= 2:
+            prev = s[0]
+            cur = s[1]
+        else:
+            return 1
+        for i in range(1, len(s)):
+            cur = s[i]
+            if cur == prev:
+                count += 1
+            else:
+                if count > max_count:
+                    max_count = count
+                count = 1
+            prev = cur
+        if count > max_count:
+            max_count = count
+            count = 1
+        return max_count
+
+
+
+if __name__ == "__main__":
+    # print(Solution().maxPower("leetcode"))
+    # print(Solution().maxPower("abbcccddddeeeeedcba"))
+    # print(Solution().maxPower("triplepillooooow"))
+    # print(Solution().maxPower("hooraaaaaaaaaaay"))
+    print(Solution().maxPower("ccbccbb"))
